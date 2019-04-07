9801617e7ab5ec67d466fa1e8d39d8f422350cd8
Eddie Jaquez <erjaquez@gmail.com>
2019-04-07 10:53:42 -0400

    Added SQL Database & Scripts - Fully functional

---
+++ bamazonData.sql
@@ -0,0 +1,16 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Apple Airpods', 'Electronics', 200.00, 5);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Apple Macbook Pro 13-inch', 'Electronics', 1800.00, 8);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('bAmazon Home Speaker', 'Electronics', 140.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('bAmazon Hoodie', 'Apparel', 60.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('bAmazon Computer Mouse', 'Electronic Accessory', 30.00, 6);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('bAmazon Notebook', 'Office', 10.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Mid-Century Modern Table', 'Furniture', 110.00, 7);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Mid-Century Modern Lamp', 'Furniture', 90.00, 24);
